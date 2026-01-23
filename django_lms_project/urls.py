31872f012cf70d101eac692f438d999ea4d76565
Swetlana Mirzoeva <levka008@mail.ru>
2026-01-23 23:49:27 +0300

    feat: add django project with basic configuration

---
+++ django_lms_project/urls.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from materials.views import CourseViewSet
+from users.views import UserViewSet
+
+router = DefaultRouter()
+router.register(r"users", UserViewSet)
+router.register(r"courses", CourseViewSet)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    path("api/lessons/", include("materials.urls")),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
